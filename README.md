afab0b09c68cd9fc83b0f6af255ffe974877e5cf
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2023-01-24 00:21:32 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # drone
 Drone project using a raspberry pi pico
+
+To install and set up cmake first have installed:
+    ARM GCC Compiler (https://developer.arm.com/downloads/-/gnu-rm)
+    CMake (https://cmake.org/download/)
+    Build Tools for Visual Studio Code (visual studio 2022)
+    Python
+    Git
+Ensure these are added to the PATH environment variable.
+
+First install pico-sdk in the same directory as the project or elsewhere and set the PICO_SDK_PATH environment variable to the path of the pico-sdk directory.:
+    ```bash
+    git clone -b master https://github.com/raspberrypi/pico-sdk.git
+    ```
+Then in pico-sdk directory add the tinyUSB submodule
+    ```bash
+    git submodule update --init
+    ```
+Then using Developer command prompt for VS2022 in the project directory run the following commands:
+    ```bash
+    mkdir build
+    cd build
+    cmake -G “NMake Makefiles” ..
+    ```
+Then to build the project run, still in the Developer command prompt for VS2022:
+    ```bash
+    nmake
+    ```
+
+To set up in Visual Studio Code:
+Ensure that the CMake Tools extension is installed.
+In Cmake Extension settings:
+    Add item to CMake.configureEnvironment:
+        PICO_SDK_PATH: <path to pico-sdk>
+    Add Cmake:Generator to:
+        NMake Makefiles
+
+Then set kit to GCC <version> arm-none-eabi
---
+++ pwm/hello_pwm.c
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// Output PWM signals on pins 0 and 1
+
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+int main() {
+
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    /// \tag::setup_pwm[]
+
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Tell GPIO 0 and 1 they are allocated to the PWM
+    gpio_set_function(0, GPIO_FUNC_PWM);
+    gpio_set_function(1, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
+    uint slice_num = pwm_gpio_to_slice_num(0);
+
+    // Set period of 4 cycles (0 to 3 inclusive)
+    pwm_set_wrap(slice_num, 3);
+    // Set channel A output high for one cycle before dropping
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 1);
+    // Set initial B output high for three cycles before dropping
+    pwm_set_chan_level(slice_num, PWM_CHAN_B, 3);
+    // Set the PWM running
+    pwm_set_enabled(slice_num, true);
+    /// \end::setup_pwm[]
+
+    // Note we could also use pwm_set_gpio_level(gpio, x) which looks up the
+    // correct slice and channel for a given GPIO.
+
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+#endif
+}
