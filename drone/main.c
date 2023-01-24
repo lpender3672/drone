491b97105d3e459bf88769d93f6a78a5c3cdf86f
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2023-01-24 20:06:50 +0000

    2nd commit

---
+++ drone/main.c
@@ -0,0 +1,15 @@
+// Drone Project
+// Authors: Louis Pender and Jacob Trevithick
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
+
+
+int main() {
+
+    printf("Hello, world! \n");
+    while (1) {}
+}
