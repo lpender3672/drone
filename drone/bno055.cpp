25677df604e83d4834d0ac560e8d40672035a52a
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-02-11 10:38:57 +0000

    added linear algreba library eigen and configured the pico debug probe

---
+++ drone/bno055.cpp
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+
+static const uint8_t ADXL343_ADDR = 0x53;
+
+static const uint8_t REG_DEVID = 0x00;
+static const uint8_t REG_POWER_CTL = 0x2D;
+static const uint8_t REG_DATAX0 = 0x32;
+
+static const uint8_t DEVID = 0xE5;
+static const float SENSITIVITY_2G = 1.0 / 256;  // (g/LSB)
+static const float EARTH_GRAVITY = 9.80665;     // Earth's gravity in [m/s^2]
+
+const uint sda_pin = 16;
+const uint scl_pin = 17;
+
+uint8_t data[6];
+
+
+int reg_write(  i2c_inst_t *i2c, 
+                const uint addr, 
+                const uint8_t reg, 
+                uint8_t *buf,
+                const uint8_t nbytes) {
+
+    int num_bytes_read = 0;
+    uint8_t msg[nbytes + 1];
+
+    // Check to make sure caller is sending 1 or more bytes
+    if (nbytes < 1) {
+        return 0;
+    }
+
+    // Append register address to front of data packet
+    msg[0] = reg;
+    for (int i = 0; i < nbytes; i++) {
+        msg[i + 1] = buf[i];
+    }
+
+    // Write data to register(s) over I2C
+    i2c_write_blocking(i2c, addr, msg, (nbytes + 1), false);
+
+    return num_bytes_read;
+}
+
+
+int reg_read(  i2c_inst_t *i2c,
+                const uint addr,
+                const uint8_t reg,
+                uint8_t *buf,
+                const uint8_t nbytes) {
+
+    int num_bytes_read = 0;
+
+    // Check to make sure caller is asking for 1 or more bytes
+    if (nbytes < 1) {
+        return 0;
+    }
+
+    // Read data from register(s) over I2C
+    i2c_write_blocking(i2c, addr, &reg, 1, true);
+    num_bytes_read = i2c_read_blocking(i2c, addr, buf, nbytes, false);
+
+    return num_bytes_read;
+}
+
+
+bool bno055_init(i2c_inst_t *i2c, const uint addr) {
+    uint8_t id;
+    reg_read(i2c, addr, REG_DEVID, &id, 1);
+    return id == DEVID;
+}
+
---
+++ drone/main.cpp
@@ -0,0 +1,58 @@
+// Drone Project
+// Authors: Louis Pender and Jacob Trevithick
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/i2c.h"
+#include <Eigen/Dense>
+
+/// \tag::hello_uart[]
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+
+
+int main() {
+    // Set up our UART with the required speed.
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    // Send out a character without any conversions
+    uart_putc_raw(UART_ID, 'A');
+
+    // Send out a character but do CR/LF conversions
+    uart_putc(UART_ID, 'B');
+
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
+
+    Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+}
